01b7a5b5dbffb71631be3e16513d16feb6edd8b6
gsingh <gopal.singh@sorint.it>
2021-09-02 22:25:22 +0200

    gg

--- src/views/Slide_5.vue
+++ src/views/Slide_5.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="slide">
-        GG
+        <div class="title">Go e gli oggetti</div>
+        <span>Parliamone scrivendo un po di codice</span>
+        <iframe src="https://giphy.com/embed/RbDKaczqWovIugyJmW" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.title{
+    font-size: 2rem;
+    font-weight: bold;
+}
+span{
+    font-size: 1.5rem;
+    margin: 1rem;
+}
+iframe{
+    border-radius: 10px;
+}
+</style>
